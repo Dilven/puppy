ce6a5332942b00b5a9ea34721cb445489da0adc1
Dilven <mateusiakkrystian@gmail.com>
2020-12-23 20:49:05 +0100

    Endpoints in internal API for search(by name etc) series, episodes, movies

--- pages/api/movie/search.ts
+++ pages/api/movie/search.ts
@@ -2,8 +2,8 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import { MovieService } from '../../../api/external-service'
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  const { query: { id } } = req
-  const movie = await MovieService.getEpisode(id as string)
+  const { query } = req
+  const movie = await MovieService.searchMovies(query)
   res.statusCode = 200
   res.setHeader('Content-Type', 'application/json')
   res.end(JSON.stringify(movie));
