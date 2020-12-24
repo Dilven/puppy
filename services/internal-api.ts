cd049470e17f3e9213f35f5430884edf49176f61
Dilven <mateusiakkrystian@gmail.com>
2020-12-24 19:52:38 +0100

    Use const resource types in queries to API

--- services/internal-api.ts
+++ services/internal-api.ts
@@ -1,3 +1,4 @@
+import { SERIES_TYPE, MOVIE_TYPE, EPISODE_TYPE } from './../constants/resource-types';
 import axios from 'axios';
 import { Episode } from '../models/episode';
 import { Series } from '../models/series';
@@ -10,30 +11,30 @@ type ResourceType = Item['Type'];
 
 const baseApiPathname = '/api/';
 
-function get(type: 'series', params: ApiGetQuery['id']): Promise<Series>
-function get(type: 'movie', params: ApiGetQuery['id']): Promise<Movie>
-function get(type: 'episode', params: ApiGetQuery['id']): Promise<Episode>
+function get(type: typeof SERIES_TYPE, params: ApiGetQuery['id']): Promise<Series>
+function get(type: typeof MOVIE_TYPE, params: ApiGetQuery['id']): Promise<Movie>
+function get(type: typeof EPISODE_TYPE, params: ApiGetQuery['id']): Promise<Episode>
 async function get(type: ResourceType, id: ApiGetQuery['id']) {
   const { data } = await axios.get<Series | Movie | Episode>(`${baseApiPathname}${type}/${id}`);
   return data;
 }
 
-function search(type: 'series', params: ApiSearchQuery): Promise<Series[]>
-function search(type: 'movie', params: ApiSearchQuery): Promise<Movie[]>
-function search(type: 'episode', params: ApiSearchQuery): Promise<Episode[]>
+function search(type: typeof SERIES_TYPE, params: ApiSearchQuery): Promise<Series[]>
+function search(type: typeof MOVIE_TYPE, params: ApiSearchQuery): Promise<Movie[]>
+function search(type: typeof EPISODE_TYPE, params: ApiSearchQuery): Promise<Episode[]>
 async function search(type: ResourceType, params: ApiSearchQuery) {
   const queryParams = getQueryParams(params);
   const { data } = await axios.get<(Series | Movie | Episode)[]>(`${baseApiPathname}${type}?${queryParams}`);
   return data;
 }
 
-const searchMovies = (params: ApiSearchQuery) => search('movie', params);
-const searchSeries = (params: ApiSearchQuery) => search('series', params);
-const searchEpisodes = (params: ApiSearchQuery) => search('episode', params);
+const searchSeries = (params: ApiSearchQuery) => search(SERIES_TYPE, params);
+const searchMovies = (params: ApiSearchQuery) => search(MOVIE_TYPE, params);
+const searchEpisodes = (params: ApiSearchQuery) => search(EPISODE_TYPE, params);
 
-const getSeries = (id: string) => get('series', id);
-const getMovie = (id: string) => get('movie', id);
-const getEpisode = (id: string) => get('episode', id);
+const getSeries = (id: string) => get(SERIES_TYPE, id);
+const getMovie = (id: string) => get(MOVIE_TYPE, id);
+const getEpisode = (id: string) => get(EPISODE_TYPE, id);
 
 export type SearchQuery = typeof searchMovies | typeof searchSeries | typeof searchEpisodes;
 export type GetQuery = typeof getMovie | typeof getSeries | typeof getEpisode;
