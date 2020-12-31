48db6c8f0faa306fb3ed394c25a2c24c8225be41
Dilven <mateusiakkrystian@gmail.com>
2020-12-31 18:10:36 +0100

    New pages for auth

---
+++ pages/auth/error.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ErrorAuth = () => (
+  <p>ErrorAuth</p>
+);
+
+export default ErrorAuth;
---
+++ pages/auth/verify-request.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const VerifyRequest = () => (
+  <p>VerifyRequest</p>
+);
+
+export default VerifyRequest;
