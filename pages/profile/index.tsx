1889335cd4a51f3b85c2c1cfaa92bd389bfd88e9
Dilven <mateusiakkrystian@gmail.com>
2020-12-31 17:46:32 +0100

    Check session on SSR for protect routes

--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -1,4 +1,13 @@
+import { GetServerSidePropsContext } from 'next';
+import { getSession } from 'next-auth/client';
 import React from 'react';
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const session = await getSession(context)
+  return {
+    props: { session }
+  };
+};
 
 const Profile = () => (
   <p>My Profile</p>
