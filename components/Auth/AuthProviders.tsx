fb62f577c2ab7349b6ef8ce341db570c10c60684
Dilven <mateusiakkrystian@gmail.com>
2020-12-31 19:07:42 +0100

    List of providers

--- components/Auth/AuthProviders.tsx
+++ components/Auth/AuthProviders.tsx
@@ -1,7 +1,8 @@
 /* eslint-disable react/button-has-type */
 import React from 'react';
-import { providers as requestProviders, signIn } from 'next-auth/client';
-
+import { providers as requestProviders } from 'next-auth/client';
+import { List, Card } from 'antd';
+import { GithubOutlined, MailOutlined } from '@ant-design/icons';
 type PromiseResolve<T> = T extends PromiseLike<infer U> ? U : T;
 
 type Providers = PromiseResolve<ReturnType<typeof requestProviders>>;
@@ -10,15 +11,33 @@ type Props = {
   providers: Providers
 }
 
+const renderIcon = (name: string) => {
+  switch (name) {
+    case 'GitHub':
+      return <GithubOutlined />;
+    default:
+      return <MailOutlined />;
+  }
+};
+
 export const AuthProviders = ({ providers }: Props) => (
   <>
-    {Object.values(providers).map((provider) => (
-      <div key={provider.name}>
-        <button onClick={() => signIn(provider.id)}>
-          Sign in with
-          {provider.name}
-        </button>
-      </div>
-    ))}
+    <List
+      grid={{
+        gutter: 16,
+        xs: 1,
+        sm: 2,
+        md: 4,
+        lg: 4,
+        xl: 6,
+        xxl: 3,
+      }}
+      dataSource={Object.values(providers || {})}
+      renderItem={({ name }) => (
+        <List.Item>
+          <Card title={`Sign in with ${name}`}>{renderIcon(name)}</Card>
+        </List.Item>
+      )}
+    />
   </>
 );
